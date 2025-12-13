9929537a20127345d60fa4991d35c3d098e94a8e
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-13 17:17:35 +0530

    Add custom exception for service-level errors

--- app/common/exceptions.py
+++ app/common/exceptions.py
@@ -1,13 +1,6 @@
-class AppError(Exception):
+class ServiceException(Exception):
     """
-    Base exception class for application errors.
+    Exception class for service-level errors
     """
     def __init__(self, message: str):
         super().__init__(message)
-        self.message = message
-
-class ValidationError(AppError):
-    """
-    Exception raised for input validation errors.
-    """
-    pass
